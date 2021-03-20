448f08c021302a9201c3097ad2681236de012133
jthanawat <kla13thanawat@gmail.com>
2021-03-20 23:59:27 +0700

    Fixed cors

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
   'mongodb+srv://klathnw:13081308@cluster0.xopql.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
