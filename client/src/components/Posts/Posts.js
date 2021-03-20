666a53d845b9b82d80278b7761072de6ced73ad0
jthanawat <kla13thanawat@gmail.com>
2021-03-20 20:42:24 +0700

    setup frontend and backend

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Post from './Post/Post';
+
+const Posts = () => {
+  return (
+    <div>
+      <h2>Posts</h2>
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
