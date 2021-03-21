0c1b6b763c3f5831e47963ed9373a0a21dbf87d5
jthanawat <kla13thanawat@gmail.com>
2021-03-21 11:57:26 +0700

    add createPost()

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Post from './Post/Post';
+import useStyles from './styles';
 
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  console.log(posts);
+
   return (
-    <div>
-      <h2>Posts</h2>
+    <>
+      <h1 className={classes.something}>Posts</h1>
       <Post />
-    </div>
+    </>
   );
 };
 
